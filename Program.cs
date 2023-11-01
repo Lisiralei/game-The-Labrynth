a2326d07604917fe36828700e27fed57b188dc8d
Lisiralei <realallerrr1@gmail.com>
2023-11-01 21:30:18 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿namespace OOPgameLbrynth
+{
+
+    class Position
+    {
+        public int x;
+        public int y;
+        public Position(int x, int y) { this.x = x; this.y = y; }
+    }
+
+    interface MovementBehaviour
+    {
+        public void Move();
+    }
+
+    interface AttackBehaviour
+    {
+        public void AttackTarget(Entity target);
+    }
+
+
+    abstract class Object
+    {
+        public int Id { get; set; }
+        public Position Position { get; set; }
+
+        public bool passable;
+
+    }
+
+
+    abstract class Entity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Position Position { get; set; }
+
+        public abstract 
+
+    }
+
+    class Projectile : Entity, MovementBehaviour
+    {
+        public void Move()
+        {
+
+        }
+    }
+
+    class Archer : Entity, MovementBehaviour, AttackBehaviour
+    {
+        public void Move()
+        {
+
+        }
+
+        public void AttackTarget(Entity target)
+        {
+
+        }
+
+
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
